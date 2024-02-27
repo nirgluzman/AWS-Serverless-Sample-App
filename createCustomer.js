22e80c0cb4df980f66b9cd332b1fca942c58651b
nirgluzman <nir.gluzman@gmail.com>
2024-02-27 10:01:05 +0100

    add createCustomer API triggered via API Gateway

---
+++ createCustomer.js
@@ -0,0 +1,27 @@
+const AWS = require('aws-sdk');
+const { v4 } = require('uuid');
+
+module.exports.handler = async event => {
+  console.log('Event=', event);
+
+  const body = JSON.parse(event.body);
+
+  const dynamodb = new AWS.DynamoDB.DocumentClient();
+
+  const putParams = {
+    TableName: process.env.DYNAMODB_CUSTOMER_TABLE,
+    Item: {
+      id: v4(),
+      name: body.name,
+      email: body.email
+    }
+  };
+
+  await dynamodb.put(putParams).promise();
+
+  console.log('Customer created');
+
+  return {
+    statusCode: 201
+  };
+};
